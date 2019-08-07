22f319bdade940c9070546fcb0625597dbdaf979
XaviTheLegend <53869023+XaviTheLegend@users.noreply.github.com>
2019-08-07 16:52:36 +0200

    Update stickers.py

--- stdplugins/stickers.py
+++ stdplugins/stickers.py
@@ -54,8 +54,8 @@ async def _(event):
     if is_a_s:
         file_ext_ns_ion = "AnimatedSticker.tgs"
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
-        packname = f"{userid}'s @AnimatedStickersGroup"
-        packshortname = f"Uni_Borg_{userid}_as"  # format: Uni_Borg_userid
+        packname = f"xᴀᴠɪᴛʜᴇʟᴇɢᴇɴᴅ ᴀɴɪᴍᴀᴛᴇᴅ"
+        packshortname = f"xᴀᴠɪᴛʜᴇʟᴇɢᴇɴᴅ_ᴀɴɪᴍᴀᴛᴇᴅ"  # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
